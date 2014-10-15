7fb47d71eeb3a5fd78f207f88ba95ff50295cd8e
liqilihaha <liqilihaha@liqilihaha@126.com>
2014-10-15 16:05:12 -0400

    03 has been done

---
+++ done/03_FindInPartiallySortedMatrix.cpp
@@ -0,0 +1,120 @@
+/*
+ *
+ */
+
+#include <stdio.h>
+#include <stdbool.h>
+bool Find(int* matrix, int rows, int columns, int key)
+{
+    bool res = false;
+    int row = 0;
+    int column = columns - 1;
+    if(matrix != NULL && rows > 0 && columns > 0)
+    {
+        //真真真真
+        while (row < rows && column >= 0) {
+            if (key > matrix[row * columns + column]) {
+                ++row;
+            }else if (key < matrix[row * columns + column]) {
+                --column;
+            }else{
+                res = true;
+                return res;
+            }
+        }
+    }
+
+    return res;
+}
+
+// ====================真====================
+void Test(const char* testName, int* matrix, int rows, int columns, int number, bool expected)
+{
+    if(testName != NULL)
+        printf("%s begins: ", testName);
+
+    bool result = Find(matrix, rows, columns, number);
+    if(result == expected)
+        printf("Passed.\n");
+    else
+        printf("Failed.\n");
+}
+
+//  1   2   8   9
+//  2   4   9   12
+//  4   7   10  13
+//  6   8   11  15
+void Test1()
+{
+    int matrix[][4] = {{1, 2, 8, 9}, {2, 4, 9, 12}, {4, 7, 10, 13}, {6, 8, 11, 15}};
+    Test("Test1", (int*)matrix, 4, 4, 7, true);
+}
+
+//  1   2   8   9
+//  2   4   9   12
+//  4   7   10  13
+//  6   8   11  15
+void Test2()
+{
+    int matrix[][4] = {{1, 2, 8, 9}, {2, 4, 9, 12}, {4, 7, 10, 13}, {6, 8, 11, 15}};
+    Test("Test2", (int*)matrix, 4, 4, 5, false);
+}
+
+//  1   2   8   9
+//  2   4   9   12
+//  4   7   10  13
+//  6   8   11  15
+void Test3()
+{
+    int matrix[][4] = {{1, 2, 8, 9}, {2, 4, 9, 12}, {4, 7, 10, 13}, {6, 8, 11, 15}};
+    Test("Test3", (int*)matrix, 4, 4, 1, true);
+}
+
+//  1   2   8   9
+//  2   4   9   12
+//  4   7   10  13
+//  6   8   11  15
+void Test4()
+{
+    int matrix[][4] = {{1, 2, 8, 9}, {2, 4, 9, 12}, {4, 7, 10, 13}, {6, 8, 11, 15}};
+    Test("Test4", (int*)matrix, 4, 4, 15, true);
+}
+
+//  1   2   8   9
+//  2   4   9   12
+//  4   7   10  13
+//  6   8   11  15
+void Test5()
+{
+    int matrix[][4] = {{1, 2, 8, 9}, {2, 4, 9, 12}, {4, 7, 10, 13}, {6, 8, 11, 15}};
+    Test("Test5", (int*)matrix, 4, 4, 0, false);
+}
+
+//  1   2   8   9
+//  2   4   9   12
+//  4   7   10  13
+//  6   8   11  15
+void Test6()
+{
+    int matrix[][4] = {{1, 2, 8, 9}, {2, 4, 9, 12}, {4, 7, 10, 13}, {6, 8, 11, 15}};
+    Test("Test6", (int*)matrix, 4, 4, 16, false);
+}
+
+void Test7()
+{
+    Test("Test7", NULL, 0, 0, 16, false);
+}
+
+int main(int argc, char* argv[])
+{
+    Test1();
+    Test2();
+    Test3();
+    Test4();
+    Test5();
+    Test6();
+    Test7();
+
+    return 0;
+}
+
