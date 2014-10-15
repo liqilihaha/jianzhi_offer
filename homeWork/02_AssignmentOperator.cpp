1b57999d1bc8b9ec4b7a5bf2116e31c75a6d6360
liqilihaha <liqilihaha@liqilihaha@126.com>
2014-10-15 17:25:03 -0400

    setup

---
+++ homeWork/02_AssignmentOperator.cpp
@@ -0,0 +1,91 @@
+#include <stdio.h>
+#include <string.h>
+#include <string>
+
+class CMyString
+{
+public:
+    CMyString(char* pData = NULL);
+    CMyString(const CMyString& str);
+    ~CMyString(void);
+
+    CMyString& operator = (const CMyString& str);
+
+    void Print();
+
+private:
+    char* m_pData;
+};
+
+
+// ====================¿¿====================
+void CMyString::Print()
+{
+    printf("%s", m_pData);
+}
+
+void Test1()
+{
+    printf("Test1 begins:\n");
+
+    char* text = "Hello world";
+
+    CMyString str1(text);
+    CMyString str2;
+    str2 = str1;
+
+    printf("The expected result is: %s.\n", text);
+
+    printf("The actual result is: ");
+    str2.Print();
+    printf(".\n");
+}
+
+void Test2()
+{
+    printf("Test2 begins:\n");
+
+    char* text = "Hello world";
+
+    CMyString str1(text);
+    str1 = str1;
+
+    printf("The expected result is: %s.\n", text);
+
+    printf("The actual result is: ");
+    str1.Print();
+    printf(".\n");
+}
+
+void Test3()
+{
+    printf("Test3 begins:\n");
+
+    char* text = "Hello world";
+
+    CMyString str1(text);
+    CMyString str2, str3;
+    str3 = str2 = str1;
+
+    printf("The expected result is: %s.\n", text);
+
+    printf("The actual result is: ");
+    str2.Print();
+    printf(".\n");
+
+    printf("The expected result is: %s.\n", text);
+
+    printf("The actual result is: ");
+    str3.Print();
+    printf(".\n");
+}
+
+int main(int argc, char* argv[])
+{
+    Test1();
+    Test2();
+    Test3();
+
+    return 0;
+}
+
